688b3c57e8a0ff1a8245373f9ea5424bcd1f378d
serdig <serdig08@gmail.com>
2023-06-26 20:26:31 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "/src/**/*.{js,ts,jsx,tsx}",
+
+  ],
+  theme: {
+    extend: {
+      colors:{
+        'red': '#A4161A',
+        'black': '#0B090A',
+        'white': '#D3D3D3',
+        'brown': '#B1A7A6'
+
+
+
+
+
+
+      }
+
+
+    },
+  },
+  plugins: [],
+}
+
